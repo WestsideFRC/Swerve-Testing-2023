9756c3f8e3dd1b82ef1ec313fbf2e33433e97d7d
AdrienChew246 <s1736607@online.houstonisd.org>
2023-02-27 20:10:05 -0600

    Example code for YAGSL

---
+++ src/main/java/frc/robot/commands/swervedrive2/auto/Autos.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.swervedrive2.auto;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import frc.robot.subsystems.swervedrive2.SwerveSubsystem;
+
+public final class Autos
+{
+
+  private Autos()
+  {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+
+  /**
+   * Example static factory for an autonomous command.
+   */
+  public static CommandBase exampleAuto(SwerveSubsystem swerve)
+  {
+    boolean               onTheFly = false; // Use the path defined in code or loaded from PathPlanner.
+    PathPlannerTrajectory example;
+    if (onTheFly)
+    {
+      // Simple path with holonomic rotation. Stationary start/end. Max velocity of 4 m/s and max accel of 3 m/s^2
+      example = PathPlanner.generatePath(
+          new PathConstraints(4, 3),
+          new PathPoint(new Translation2d(0, 0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)),
+// position, heading(direction of travel), holonomic rotation
+          new PathPoint(new Translation2d(3, 5), Rotation2d.fromDegrees(90), Rotation2d.fromDegrees(90)),
+// position, heading(direction of travel), holonomic rotation
+          new PathPoint(new Translation2d(5, 5), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0))
+          // position, heading(direction of travel), holonomic rotation
+                                        );
+    } else
+    {
+      example = PathPlanner.loadPath("SamplePath", new PathConstraints(4, 3));
+    }
+//    swerve.postTrajectory(example);
+    return Commands.sequence(new FollowTrajectory(swerve, example, true));
+  }
+
+  public static CommandBase driveAndSpin(SwerveSubsystem swerve)
+  {
+    return Commands.sequence(
+        new RepeatCommand(new InstantCommand(() -> swerve.drive(new Translation2d(1, 0), 5, true, true), swerve)));
+  }
+}
